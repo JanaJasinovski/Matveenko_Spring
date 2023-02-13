e285bd8aa647683e6f949f89817f2bdbbae83c04
jana <jantschick3@gmail.com>
2023-02-13 14:58:25 +0300

    java_based

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -1,9 +1,10 @@
 package org.example;
 
+import org.example.config.ApplicationConfiguration;
 import org.example.database.pool.ConnectionPool;
 import org.example.database.repository.CrudRepository;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.Serializable;
 
@@ -14,8 +15,8 @@ public class ApplicationRunner {
         System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
         System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
 
-        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
-            var connectionPool = context.getBean("p1", ConnectionPool.class);
+        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
+            var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
 
             var companyRepository = context.getBean("companyRepository", CrudRepository.class);
--- src/main/java/org/example/config/ApplicationConfiguration.java
+++ src/main/java/org/example/config/ApplicationConfiguration.java
@@ -1,7 +1,21 @@
 package org.example.config;
 
+import org.example.database.repository.CrudRepository;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "org.example",
+useDefaultFilters = false,
+includeFilters = {
+        @Filter(type = FilterType.ANNOTATION, value = Component.class),
+        @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+        @Filter(type = FilterType.REGEX, pattern = "org\\..+Repository")
+})
 public class ApplicationConfiguration {
 }
