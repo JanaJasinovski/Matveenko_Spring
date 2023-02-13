850d711cf88f681dc549124832cc3b3919fdba35
jana <jantschick3@gmail.com>
2023-02-13 10:29:02 +0300

    constructor_injection

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        System.out.println(context.getBean("pool2", ConnectionPool.class));
+        ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
+         System.out.println(connectionPool);
     }
 }
