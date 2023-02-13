78c909575ca6ce2447c63346d57528e6d8ac29a3
jana <jantschick3@gmail.com>
2023-02-13 11:47:40 +0300

    property_injection

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -6,10 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
-         System.out.println(connectionPool);
-        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
+            ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
+            System.out.println(connectionPool);
+            CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 }
