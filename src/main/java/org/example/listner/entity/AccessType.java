2bad00110200f89af574fab9dd8e0301362822ff
jana <jantschick3@gmail.com>
2023-02-13 15:52:06 +0300

    event_listener

---
+++ src/main/java/org/example/listner/entity/AccessType.java
@@ -0,0 +1,5 @@
+package org.example.listner.entity;
+
+public enum AccessType {
+    CREATE, UPDATE, READ, DELETE
+}
--- src/main/java/org/example/service/CompanyService.java
+++ src/main/java/org/example/service/CompanyService.java
@@ -1,12 +1,33 @@
 package org.example.service;
 
+import org.example.database.entity.Company;
+import org.example.database.repository.CrudRepository;
+import org.example.dto.CompanyReadDto;
+import org.example.listner.entity.AccessType;
+import org.example.listner.entity.EntityEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
+    private final CrudRepository<Integer, Company> companyCrudRepository;
     private final UserService userService;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public CompanyService(UserService userService) {
+    public CompanyService(CrudRepository<Integer, Company> companyCrudRepository, UserService userService, ApplicationEventPublisher eventPublisher) {
+        this.companyCrudRepository = companyCrudRepository;
         this.userService = userService;
+        this.eventPublisher = eventPublisher;
+    }
+
+    public Optional<CompanyReadDto> findById(Integer id) {
+        return companyCrudRepository.findById(id)
+                .map(entity -> {
+                            eventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
+                            return new CompanyReadDto(entity.id());
+                        }
+                );
     }
 }
