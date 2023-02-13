0ac2bf2426f72b046424de79d84d0097f2ef4e90
jana <jantschick3@gmail.com>
2023-02-13 14:37:18 +0300

    bean definition

--- src/main/java/org/example/database/repository/CompanyRepository.java
+++ src/main/java/org/example/database/repository/CompanyRepository.java
@@ -8,6 +8,7 @@ import org.example.database.pool.ConnectionPool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
@@ -15,7 +16,7 @@ import javax.annotation.Resource;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@Scope("prototype")
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
