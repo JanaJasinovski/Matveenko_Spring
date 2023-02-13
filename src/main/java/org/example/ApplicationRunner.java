4ac774a44e6ae800e5af64fc47ba162a4d9fe61d
jana <jantschick3@gmail.com>
2023-02-13 10:12:27 +0300

    xml_based

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -1,23 +1,11 @@
 package org.example;
 
 import org.example.database.pool.ConnectionPool;
-import org.example.database.repository.CompanyRepository;
-import org.example.database.repository.UserRepository;
-import org.example.ioc.Container;
-import org.example.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        Container container = new Container();
-
-//        ConnectionPool connectionPool = new ConnectionPool();
-//        UserRepository userRepository = new UserRepository(connectionPool);
-//        CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-//        UserService userService = new UserService(userRepository, companyRepository);
-
-//        ConnectionPool connectionPool = container.get(ConnectionPool.class);
-//        UserRepository userRepository = container.get(UserRepository.class);
-//        CompanyRepository companyRepository = container.get(CompanyRepository.class);
-        UserService userService = container.get(UserService.class);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        System.out.println(context.getBean("pool2", ConnectionPool.class));
     }
 }
