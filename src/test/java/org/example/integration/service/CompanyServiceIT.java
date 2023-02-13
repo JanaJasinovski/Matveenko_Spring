903dbd1a3b28cf273ac90fc58203e39bc9188e68
jana <jantschick3@gmail.com>
2023-02-13 18:55:14 +0300

    tests

--- src/test/java/org/example/integration/service/CompanyServiceIT.java
+++ src/test/java/org/example/integration/service/CompanyServiceIT.java
@@ -1,63 +1,38 @@
 package org.example.integration.service;
 
-import org.example.ApplicationRunner;
-import org.example.database.entity.Company;
-import org.example.database.repository.CrudRepository;
+import lombok.RequiredArgsConstructor;
+import org.example.config.DatabaseProperties;
 import org.example.dto.CompanyReadDto;
-import org.example.listner.entity.EntityEvent;
+import org.example.integration.annotation.IT;
 import org.example.service.CompanyService;
-import org.example.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
 
-import java.util.Optional;
-
-import static org.hamcrest.Matchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-@ExtendWith(MockitoExtension.class)
-@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
+@IT
+@RequiredArgsConstructor
+//@ExtendWith(MockitoExtension.class)
+//@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
 class CompanyServiceIT {
+
+    private static final Integer COMPANY_ID = 1;
+
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
+        var actualResult = companyService.findById(COMPANY_ID);
 
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
     }
 
-//    private static final Integer COMPANY_ID = 1;
-//
-//    @Mock
-//    private CrudRepository<Integer, Company> companyRepository;
-//    @Mock
-//    private UserService userService;
-//    @Mock
-//    private ApplicationEventPublisher eventPublisher;
-//    @InjectMocks
-//    private CompanyService companyService;
-//
-//    @Test
-//    void findById() {
-//        doReturn(Optional.of(new Company(COMPANY_ID)))
-//                .when(companyRepository).findById(COMPANY_ID);
-//
-//        var actualResult = companyService.findById(COMPANY_ID);
-//
-//        assertTrue(actualResult.isPresent());
-//
-//        var expectedResult = new CompanyReadDto(COMPANY_ID);
-//        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
-//
-//        verify(eventPublisher).publishEvent(any(EntityEvent.class));
-//        verifyNoMoreInteractions(eventPublisher, userService);
-//    }
 }
